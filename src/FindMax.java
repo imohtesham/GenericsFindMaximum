ebd209bbb052df3c2fb4d1cba2b81ef25bd3f23c
imohtesham <ibraahimm92@gmail.com>
2022-10-19 15:52:01 +0530

    find max string value uc 3

--- src/FindMax.java
+++ src/FindMax.java
@@ -19,6 +19,14 @@ public class FindMax {
         else return num6;
     }
 
+    public String findMaxString(String str1, String str2, String str3){
+        if(str1.compareTo(str2) > 0  && str1.compareTo(str2) > 0)
+            return str1;
+        else if(str2.compareTo(str3) > 0)
+            return str2;
+        else return str3;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to the Find Maximum Program");
         FindMax findMax = new FindMax();
@@ -34,6 +42,12 @@ public class FindMax {
         Float num5 = sc.nextFloat();
         Float num6 = sc.nextFloat();
         System.out.println("Max Float value : " + findMax.findMaxFloat(num4, num5, num6));
+
+        System.out.println("Enter the three strings : ");
+        String str1 = sc.next();
+        String str2 = sc.next();
+        String str3 = sc.next();
+        System.out.println("Max String value : " + findMax.findMaxString(str1, str2, str3));
 
     }
 }
